c89f692bc4b5afc7948d78606d9c4be4ad61dade
cmorten <craig.morten@hotmail.co.uk>
2021-03-17 21:58:00 +0000

    fix: formatting

--- src/middlewares/transform/plugins/esbuild.ts
+++ src/middlewares/transform/plugins/esbuild.ts
@@ -11,7 +11,7 @@ let esbuildReady = false;
 export function esbuild() {
   if (!esbuildInitialized) {
     esbuildInitialized = true;
-    
+
     const esbuildInitializePromise = esbuildInstance.initialize({
       worker: false,
       wasmURL: "https://esm.sh/esbuild-wasm@0.9.3/esbuild.wasm",
