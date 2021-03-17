6378d3071461a80214a2449bfa0484684d27e099
cmorten <craig.morten@hotmail.co.uk>
2021-03-17 17:25:49 +0000

    feat: placeholder for build CLI command

--- src/cli/mod.ts
+++ src/cli/mod.ts
@@ -9,19 +9,33 @@ interface ServeOptions {
 
 const DEFAULT_PORT = 3000;
 
+function notImplemented() {
+  console.log("Not Implemented");
+}
+
 async function serve({ port, hostname }: ServeOptions, root: string) {
   await server({ root, server: { port: port ?? DEFAULT_PORT, hostname } });
 }
 
-await new Command()
+async function build() {
+  notImplemented();
+}
+
+const program = await new Command()
   .name("luath")
   .version(version)
-  .description("CLI for fast front-end development in Deno")
-  .command("serve <root:string>")
-  .description("Serve an application directory with HMR")
+  .description("CLI for fast front-end development in Deno");
+
+program.command("serve <root:string>")
+  .description("Serve an application directory with HMR [WIP]")
   .option("-p, --port <port:number>", "Port to run the server on", {
     default: DEFAULT_PORT,
   })
   .option("-h, --hostname <hostname:string>", "Hostname to run the server on")
-  .action(serve)
-  .parse(Deno.args);
+  .action(serve);
+
+program.command("build <root:string>")
+  .description("Build an application directory [Not Implemented]")
+  .action(build);
+
+program.parse(Deno.args);
